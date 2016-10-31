e53199f22b102a10626c3ba318d762d8f1c5e78c
Laith Sinawi <laith@laithsinawi.com>
2016-10-31 15:41:46 -0700

    Initial commit for version 2.0 -  rebuilding everything from the ground up

---
+++ includes/class-fancy-grid-portfolio-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.sinawiwebdesign.com
+ * @since      1.0.0
+ *
+ * @package    Fancy_Grid_Portfolio
+ * @subpackage Fancy_Grid_Portfolio/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Fancy_Grid_Portfolio
+ * @subpackage Fancy_Grid_Portfolio/includes
+ * @author     Laith Sinawi <info@sinawiwebdesign.com>
+ */
+class Fancy_Grid_Portfolio_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ includes/class-fancy-grid-portfolio-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       http://www.sinawiwebdesign.com
+ * @since      1.0.0
+ *
+ * @package    Fancy_Grid_Portfolio
+ * @subpackage Fancy_Grid_Portfolio/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Fancy_Grid_Portfolio
+ * @subpackage Fancy_Grid_Portfolio/includes
+ * @author     Laith Sinawi <info@sinawiwebdesign.com>
+ */
+class Fancy_Grid_Portfolio_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'fancy-grid-portfolio',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
